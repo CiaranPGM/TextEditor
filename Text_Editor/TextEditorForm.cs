ed95dcc46d2def32bcde49a5484bbd546dace3b2
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-10-10 01:56:56 +1100

    Added classes

--- Text_Editor/TextEditorForm.cs
+++ Text_Editor/TextEditorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Text_Editor
 {
-    public partial class Form1 : Form
+    public partial class TextEditorForm : Form
     {
-        public Form1()
+        public TextEditorForm()
         {
             InitializeComponent();
         }
